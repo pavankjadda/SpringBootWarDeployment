f9cb982a8163bdc9b732dbf1734ed6a02f808814
Pavan Jadda <jpavanaryan@gmail.com>
2019-06-14 12:20:13 -0400

    Updated

--- src/main/java/com/pj/springdatademo/repo/CategoryRepository.java
+++ src/main/java/com/pj/springdatademo/repo/CategoryRepository.java
@@ -2,15 +2,12 @@ package com.pj.springdatademo.repo;
 
 import com.pj.springdatademo.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category,Long>,CategoryRepositoryCustom
 {
 
-    @Query(name = "CALL get_all_categories;", nativeQuery = true)
-    List<Category> getAllCategoriesThroughStoredProcedure();
+    /*@Query(name = "CALL get_all_categories;", nativeQuery = true)
+    List<Category> getAllCategoriesThroughStoredProcedure();*/
 
     /*
         @Procedure(procedureName="get_all_categories")
