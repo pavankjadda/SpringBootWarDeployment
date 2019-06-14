baad958791a2a22e420c48764886c5de2637abfa
Pavan Jadda <jpavanaryan@gmail.com>
2019-06-14 12:32:31 -0400

    War packaging works

--- src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
+++ src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
@@ -3,13 +3,21 @@ package com.pj.springdatademo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class SpringDataDemoApplication
+public class SpringDataDemoApplication extends SpringBootServletInitializer
 {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(SpringDataDemoApplication.class);
+    }
 
     public static void main(String[] args)
     {
