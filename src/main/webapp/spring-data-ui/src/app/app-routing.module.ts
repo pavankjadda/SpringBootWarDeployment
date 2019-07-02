617dfbc49575ba76a0937c7aa42076c5d993eafc
Pavan Jadda <jpavanaryan@gmail.com>
2019-07-01 22:17:07 -0400

    Updated UI for Zuul testing

--- src/main/webapp/spring-data-ui/src/app/app-routing.module.ts
+++ src/main/webapp/spring-data-ui/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {CategoryListComponent} from "./category-list/category-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'category/list',
+    component: CategoryListComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
---
+++ src/main/webapp/spring-data-ui/src/app/category-list/category-list.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {Category} from "../category";
+import {CategoryService} from "./category.service";
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css']
+})
+export class CategoryListComponent implements OnInit
+{
+  categories: Array<Category>;
+
+  constructor(private router:Router,
+              private categoryService:CategoryService)
+  { }
+
+  ngOnInit()
+  {
+    this.loadCategories();
+  }
+
+  private loadCategories()
+  {
+     this.categoryService.loadCategories('http://localhost:8080/category/list').subscribe(
+       data=> {
+         this.categories=data;
+       }
+     );
+
+  }
+}
