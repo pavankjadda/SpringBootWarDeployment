4c2c2fa288aa7eb483c4bf8fad47bdefbba75fa7
Pavan Jadda <jpavanaryan@gmail.com>
2019-07-01 22:43:59 -0400

    Updated UI for Zuul testing

--- src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
+++ src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -19,5 +21,20 @@ public class SpringDataDemoApplication
     public ModelMapper modelMapper()
     {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings(CorsRegistry registry)
+            {
+                registry.addMapping("/category/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST");
+            }
+        };
     }
 }
