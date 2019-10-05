6803b2725baab7cf0d71f4fbbeb4eec7d4c7d5d8
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-10-04 23:36:08 -0400

    Upgrade to Angular 8.3.1

--- src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
+++ src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
@@ -3,11 +3,12 @@ package com.pj.springdatademo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class SpringDataDemoApplication
+public class SpringDataDemoApplication extends SpringBootServletInitializer
 {
     public static void main(String[] args)
     {
