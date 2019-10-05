c90acd9861896cc4fa0670d40ba33733f1f81389
Pavan Jadda <jpavanaryan@gmail.com>
2019-10-04 23:35:47 -0400

    Before Upgrade

---
+++ src/main/webapp/spring-data-ui/upgrade.sh
@@ -0,0 +1,15 @@
+##Before upgrade
+git add .
+git commit -m "Before Upgrade"
+git push origin war-deployment
+
+## Upgrade
+ng update --all --force
+npm install zone.js@0.9.1 typescript@3.5.3 --save
+npm install
+
+
+## After upgrade
+git add .
+git commit -m "After Upgrade"
+git push origin  war-deployment
